a29c0c726ad3ea1e4227b8082b9c7f5509a25af8
Maciel Júnior <macielfcjunior@icloud.com>
2022-06-25 20:58:12 -0300

    :sparkles: feat: Crias as rotas login, clientes, fornecedores e produtos

---
+++ resources/views/site/sobre-nos.blade.php
@@ -0,0 +1,9 @@
+<h3>Sobre Nós (view)</h3>
+
+<nav>
+  <ul>
+    <li><a href=" {{ route('site.index') }} ">Principal</a></li>
+    <li><a href=" {{ route('site.sobrenos') }} ">Sobre Nós</a></li>
+    <li><a href=" {{ route('site.contato') }} ">Contato</a></li>
+  </ul>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,18 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get("/", [PrincipalController::class, "principal"])->name('site.index');
+Route::get("/sobre-nos", [SobreNosController::class, "sobreNos"])->name("site.sobrenos");
+Route::get("/contato", [ContatoController::class, "contato"])->name("site.contato");
 
-Route::get("/", [PrincipalController::class, "principal"]);
-Route::get("/sobre-nos", [SobreNosController::class, "sobreNos"]);
-Route::get("/contato", [ContatoController::class, "contato"]);
+Route::get("/login", [LoginController::class, "contato"])->name("site.login");
+
+Route::prefix("/app")->group(function () {
+    Route::get("/clientes", [ClientesController::class, "clientes"])->name("app.clientes");
+    Route::get("/forncecedores", [FornecedoresController::class, "fornecedores"])->name("app.fornecedores");
+    Route::get("/produtos", [ProdutosController::class, "produtos"])->name("app.produtos");
+});
+
+Route::fallback(function () {
+    echo "A rota acessada não existe. <a href='" . route('site.index') . "'>Clique aqui para voltar a página inicial</a>";
+});
