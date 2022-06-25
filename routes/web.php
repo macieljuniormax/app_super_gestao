552feb0032b4485031fe3baf34a2cfc58bdac4cf
Maciel Júnior <macielfcjunior@icloud.com>
2022-06-25 14:50:24 -0300

    :sparkles: feat: Cria as primeiras rotas (Sobre nós e Contato)

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return "Olá, seja bem-vindo";
+});
+
+Route::get("/sobre-nos", function () {
+    return "Sobre nós";
+});
+
+Route::get("/contato", function () {
+    return "Contato";
 });
